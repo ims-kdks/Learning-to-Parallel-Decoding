23299d88119d8c6390d211a278d3d8b4892c5919
Zhiben Chen <imarod@outlook.com>
2025-10-26 19:32:11 +0000

    update eval.sh

--- dream/eval.sh
+++ dream/eval.sh
@@ -8,7 +8,6 @@ export CUDA_VISIBLE_DEVICES=0,1,2,3
 task=gsm8k
 gen_length=256
 block_length=32
-steps=$((gen_length / block_length))
 model="Dream-org/Dream-v0-Instruct-7B"
 method=original
 accelerate launch eval.py --model dream \
@@ -18,12 +17,9 @@ accelerate launch eval.py --model dream \
     --confirm_run_unsafe_code \
     > output/eval_results/$task/eval_${method}_${gen_length}.log 2>&1
 
-sleep 3600
-
 task=gsm8k
 gen_length=256
 block_length=32
-steps=$((gen_length / block_length))
 model="Dream-org/Dream-v0-Instruct-7B"
 method=EoT
 accelerate launch eval.py --model dream \
@@ -33,10 +29,22 @@ accelerate launch eval.py --model dream \
     --confirm_run_unsafe_code \
     > output/eval_results/$task/eval_${method}_${gen_length}.log 2>&1
 
+task=gsm8k
+gen_length=256
+block_length=32
+model="Dream-org/Dream-v0-Instruct-7B"
+method=L2P
+accept_thres=0.9
+accelerate launch eval.py --model dream \
+    --model_args pretrained=${model},max_new_tokens=${gen_length},diffusion_steps=${gen_length},add_bos_token=true,alg=entropy,show_speed=True,block_length=${block_length},method=${method},accept_thres=${accept_thres} \
+    --tasks ${task} \
+    --batch_size 1 \
+    --confirm_run_unsafe_code \
+    > output/eval_results/$task/eval_${method}_${gen_length}_${accept_thres//./}.log 2>&1
+
 # task=humaneval
 # gen_length=256
 # block_length=32
-# steps=$((gen_length / block_length))
 # model="Dream-org/Dream-v0-Base-7B"
 # method=original
 
